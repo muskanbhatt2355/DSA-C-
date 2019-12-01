fe4e62d714bf79577b6f43565a473bc078746b39
muskanbhatt2355 <56234099+muskanbhatt2355@users.noreply.github.com>
2019-12-01 18:31:26 +0530

    Insertion Sort in cpp

---
+++ insertionFinal.cpp
@@ -0,0 +1,36 @@
+#include<simplecpp>
+
+void insert_sort(int Array[],int n){
+    int j,i,key;
+
+    for(int i=1;i<n;i++){
+        key=Array[i];
+        j=i-1;
+        while(j>=0&&Array[j]>key){
+            Array[j+1]=Array[j];
+            Array[j]=key;
+            j=j-1;
+        }
+    }
+}
+
+
+void print_array(int Array[],int n){
+    for(int i=0;i<n;i++){
+        cout<<Array[i]<<" ";
+        }
+}
+
+main_program{
+
+int n;
+cin>>n;
+
+int Array[n];
+
+for(int i=0;i<n;i++){
+    cin>>Array[i];
+    }
+insert_sort(Array,n);
+print_array(Array,n);
+}
